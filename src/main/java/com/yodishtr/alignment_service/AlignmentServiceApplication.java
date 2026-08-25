8088b0826231555bbaf9d0e9ded09df96b880639
vythili6 <yo.vythilingum@mail.utoronto.ca>
2026-08-24 20:38:24 -0400

    Initial commit with gitignore

---
+++ src/main/java/com/yodishtr/alignment_service/AlignmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yodishtr.alignment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlignmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlignmentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yodishtr/alignment_service/AlignmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yodishtr.alignment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlignmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
